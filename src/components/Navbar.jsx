17b52bbb8f12fe3490064c91044dc6e37f24aab5
Sourav Chowdhury <souravchowdhurybnk@gmail.com>
2024-10-31 23:06:15 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import { setSearch } from "../redux/slices/SearchSlice";
+
+const Navbar = () => {
+  const dispatch = useDispatch();
+  return (
+    <nav className="flex flex-col lg:flex-row justify-between py-3 mx-6 mb-10">
+      <div>
+        <h3 className="text-xl font-bold text-pink-500">
+          {new Date().toUTCString().slice(0, 16)}
+        </h3>
+        <h1 className="text-2xl font-bold text-pink-500">Flavoro Foods</h1>
+      </div>
+      <div>
+        <input
+          type="search"
+          name="search"
+          id=""
+          placeholder="Search here"
+          autoComplete="off"
+          onChange={(e) => dispatch(setSearch(e.target.value))}
+          className="p-3 border border-pink-500 text-sm rounded-lg outline-none w-full lg:w-[25vw]"
+        />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
